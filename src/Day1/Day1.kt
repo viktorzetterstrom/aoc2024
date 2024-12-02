05b78fff49cf596098a7f1f0ced9134e4e8d0c47
Viktor Zetterström <viktor.zetterstrom@sanalabs.com>
2024-12-02 08:05:14 +0100

    day 1

---
+++ src/Day1/Day1.kt
@@ -0,0 +1,28 @@
+package Day1
+
+import java.io.File
+import kotlin.math.abs
+
+private fun main() {
+    val fileContent = File("src/Day1/input.txt").readText()
+    val lines = fileContent.split("\n")
+
+    val (left, right) = lines.map {
+        val (l, r)  = it.split("\\s+".toRegex())
+        Pair(l.toInt(), r.toInt())
+    }.unzip()
+
+    val sortedLeft = left.sorted()
+    val sortedRight = right.sorted()
+
+    val part1 = sortedLeft.zip(sortedRight).sumOf {
+        abs(it.first - it.second)
+    }
+    println(part1)
+
+
+    val part2 = left.sumOf { l ->
+        l *  right.count { it == l }
+    }
+    println(part2)
+}
